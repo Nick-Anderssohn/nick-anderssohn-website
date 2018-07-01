02adf23d11161983d43fa9c478599aebf9691f3e
Nick Anderssohn <nicholas.anderssohn@outlook.com>
2018-07-01 01:14:00 -0700

    created vol for downloads

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ docker image build -t dart-builder .
 cwd=$(pwd)
 docker run --name full-share-dart-builder -v $cwd:/builder dart-builder /bin/bash -c "pub get; pub build"
 docker rm full-share-dart-builder
+
+mkdir build/web/download
