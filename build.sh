08f59dd01108bfa657850df2027c2847d28de525
Nick Anderssohn <nicholas.anderssohn@outlook.com>
2018-07-01 00:51:01 -0700

    able to build dart code using docker

---
+++ build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cd web
+docker image build -t dart-builder .
+cwd=$(pwd)
+docker run --name full-share-dart-builder -v $cwd:/builder dart-builder /bin/bash -c "pub get; pub build"
+docker rm full-share-dart-builder
